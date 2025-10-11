b135bfc367ca2c025f58907e3ba15310b3b9a496
Fourth <fourthe.hero@gmail.com>
2025-10-11 23:16:04 +0700

    feat: create tier template table

---
+++ internal/core/entities/tier_template.go
@@ -0,0 +1,19 @@
+package entities
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type TierTemplate struct {
+	ID          uuid.UUID `gorm:"primarykey"`
+	Name        string    `json:"name"`
+	Type        string
+	PlayedCount uint
+	TierList    map[string]interface{} `gorm:"serializer:json"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	DeletedAt   gorm.DeletedAt `gorm:"index"`
+}
